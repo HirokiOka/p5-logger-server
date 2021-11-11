299c518c750d0838247851bcdaf1a7ec4103b29e
hiroki <hiro_bass.0827@icloud.com>
2021-11-11 16:30:10 +0900

    add psql connection

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 const express = require('express');
-//const pg = require('pg');
+const { Client } = require('pg');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dbClient = new Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: porocess.env.DB,
+  password: process.env.PWD,
+  port: process.env.DBPORT 
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
   origin: ['https://editor.p5js.org']
 }));
 
+dbClient.connect().then(() => console.log('DB connected'));
+
 app.post('/data', async (req, res) => {
-  console.log(req.body);
-  const postedData = JSON.stringify(req.body);
-  fs.appendFile('./p5log.json', postedData, (err) => {
-    if (err) throw err;
-    console.log('data appended');
+  const id = parseInt(req.body.userId);
+  console.log(id);
+  const timestamp = req.body.timestamp;
+  const code = req.body.code;
+  const query = {
+    text: 'INSERT INTO p5log (id, timestamp, code) VALUES($1, $2, $3)',
+    values: [id, timestamp, code],
+  };
+
+  dbClient.query(query, (err, res) => {
+    console.log(err, res);
   });
   res.json({ "message": "success" });
 });
